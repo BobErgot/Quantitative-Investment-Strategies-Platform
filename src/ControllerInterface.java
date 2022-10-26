553e38ac251870e998b087bb705f344dc55f903b
harsh4799 <helloharshit68@gmail.com>
2022-10-26 18:05:41 -0400

    Add API Interface, ControllerInterface

--- src/ControllerInterface.java
+++ src/ControllerInterface.java
@@ -1,3 +1,8 @@
-public interface ControllerInterface {
+import java.util.function.Predicate;
 
+public interface ControllerInterface {
+  void createPortfolio();
+  void viewPortfolio();
+  // Filter function
+  <T> void conditionalViewPortfolio(Predicate<T> filter);
 }
---
+++ src/model/APIInterface.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Date;
+
+// Not useful at the moment
+public interface APIInterface {
+
+  double getShareValueByGivenDate(String stockSymbol, Date date);
+
+}
