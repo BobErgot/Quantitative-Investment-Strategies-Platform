11ccdcb280716909a6a50370e07d2b60e1a2f952
BobErgot <25260745+BobErgot@users.noreply.github.com>
2022-10-27 11:10:20 -0400

    Add API interface implementation

---
+++ src/model/FileDatabase.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Date;
+
+public class FileDatabase implements APIInterface{
+  @Override
+  public double getShareValueByGivenDate(String stockSymbol, Date date) {
+    return 0;
+  }
+}
---
+++ src/model/WebAPI.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Date;
+
+public class WebAPI implements APIInterface{
+  @Override
+  public double getShareValueByGivenDate(String stockSymbol, Date date) {
+    return 0;
+  }
+}
