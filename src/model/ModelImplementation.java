56abe3572a93513c33481493d7ba71c15bf51b53
BobErgot <25260745+BobErgot@users.noreply.github.com>
2022-11-01 15:35:53 -0400

    Modify portfolio

--- src/model/ModelImplementation.java
+++ src/model/ModelImplementation.java
@@ -11,7 +11,6 @@ import java.util.function.Predicate;
 import static utility.Constants.PORTFOLIO_DIRECTORY;
 
 public class ModelImplementation implements ModelInterface {
-  private int id=0;
   private List<Share> shares;
   // maintains last portfolio used for faster inference
   private Portfolio portfolioCache;
@@ -20,20 +19,18 @@ public class ModelImplementation implements ModelInterface {
 
   }
   @Override
-  public void createPortfolio() {
-    // remember to delete share list after creating portfolio
-    Portfolio portfolioObject = new Portfolio(id++,new HashSet<>(shares), LocalDate.now());
-    //System.out.println(portfolioObject.toString());
+  public void createPortfolio(String portfolioName) {
+    Portfolio portfolioObject = new Portfolio(portfolioName,new HashSet<>(shares), LocalDate.now());
     FileInterface fileDatabase = new CSVFile();
     String formattedString = fileDatabase.convertObjectListIntoString(shares);
     String shareFileName = UUID.randomUUID().toString();
     List<String> referenceList = new ArrayList<>();
     referenceList.add(shareFileName);
     if (fileDatabase.writeToFile(PORTFOLIO_DIRECTORY, shareFileName ,formattedString.getBytes())){
-      //System.out.println(formattedString);
       fileDatabase.writeToFile(PORTFOLIO_DIRECTORY, PORTFOLIO_DIRECTORY,
               fileDatabase.convertObjectIntoString(portfolioObject.toString(), referenceList).getBytes());
     }
+    // remember to delete share list after creating portfolio
     shares = new ArrayList<>();
     portfolioCache = portfolioObject;
   }
