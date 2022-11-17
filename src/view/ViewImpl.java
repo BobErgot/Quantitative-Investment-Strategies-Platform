0fb0b1f2a5a6f133aa5064130a1cfe54b08291ca
Harshit Pandey <35086260+harsh4799@users.noreply.github.com>
2022-11-17 15:39:52 -0500

    Println to append

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -1,14 +1,14 @@
 package view;
 
+import static utility.Constants.FILE_SEPARATOR;
+import static utility.Constants.LINE_BREAKER;
+
 import java.io.PrintStream;
 import java.time.LocalDate;
 import java.util.EnumSet;
+import java.util.Formatter;
 import java.util.List;
-
 import model.Periodicity;
-
-import static utility.Constants.FILE_SEPARATOR;
-import static utility.Constants.LINE_BREAKER;
 
 /**
  * The controller implementation that receives all its inputs from an InputStream object and
@@ -30,33 +30,33 @@ public class ViewImpl implements View {
 
   @Override
   public void showMainMenu() {
-    this.out.println("* Home Menu:");
-    this.out.println("1. Create Portfolio");
-    this.out.println("2. Upload Portfolio from given path");
-    this.out.println("3. View Portfolio");
-    this.out.println("Type 'quit' or 'exit' to close the program");
-    this.out.println("Please select an option from 1-x from above: ");
+    this.out.append(LINE_BREAKER + "* Home Menu:");
+    this.out.append(LINE_BREAKER + "1. Create Portfolio");
+    this.out.append(LINE_BREAKER + "2. Upload Portfolio from given path");
+    this.out.append(LINE_BREAKER + "3. View Portfolio");
+    this.out.append(LINE_BREAKER + "Type 'quit' or 'exit' to close the program");
+    this.out.append(LINE_BREAKER + "Please select an option from 1-x from above: ");
   }
 
   @Override
   public void showCreatePortfolioMenu(boolean canCreateShare) {
-    this.out.println("* Create Portfolio Menu:");
-    this.out.println("1. Add Shares");
+    this.out.append(LINE_BREAKER + "* Create Portfolio Menu:");
+    this.out.append(LINE_BREAKER + "1. Add Shares");
     if (canCreateShare) {
-      this.out.println("2. Create Portfolio (Finalize current Portfolio)");
+      this.out.append(LINE_BREAKER + "2. Create Portfolio (Finalize current Portfolio)");
     }
-    this.out.println("Type 'back' to return to Home Menu");
-    this.out.println("Please select an option from 1-x from above: ");
+    this.out.append(LINE_BREAKER + "Type 'back' to return to Home Menu");
+    this.out.append(LINE_BREAKER + "Please select an option from 1-x from above: ");
   }
 
   @Override
   public void showAddShareWithApiInputMenu(int parameterNumber) throws IllegalArgumentException {
     switch (parameterNumber) {
       case 0:
-        this.out.println("Enter Company Name:");
+        this.out.append(LINE_BREAKER + "Enter Company Name:");
         break;
       case 1:
-        this.out.println("Enter Number of shares:");
+        this.out.append(LINE_BREAKER + "Enter Number of shares:");
         break;
       default:
         throw new IllegalArgumentException("Incorrect parameter Number");
@@ -65,154 +65,159 @@ public class ViewImpl implements View {
 
   @Override
   public void showViewPortfolioMenu(List<String> portfolioList) {
-    this.out.println("Portfolios available:");
-    this.out.println("--------------------------------------------------------------"
-            + "----------------------");
+    this.out.append(LINE_BREAKER + "Portfolios available:");
+    this.out.append(LINE_BREAKER + "--------------------------------------------------------------"
+        + "----------------------");
     String portfolioHeaderString = String.format("||%-18s||%-40s||%-18s||", "Serial Number",
-            "Portfolio Name", "Creation Date");
-    this.out.println(portfolioHeaderString);
-    this.out.println("||------------------||----------------------------------------"
-            + "||------------------||");
+        "Portfolio Name", "Creation Date");
+    this.out.append(LINE_BREAKER + portfolioHeaderString);
+    this.out.append(LINE_BREAKER + "||------------------||----------------------------------------"
+        + "||------------------||");
     for (int i = 0; i < portfolioList.size() - 1; i++) {
-      this.out.println(portfolioList.get(i));
-      this.out.println("||------------------||----------------------------------------"
+      this.out.append(LINE_BREAKER + portfolioList.get(i));
+      this.out.append(
+          LINE_BREAKER + "||------------------||----------------------------------------"
               + "||------------------||");
     }
-    this.out.println(portfolioList.get(portfolioList.size() - 1));
-    this.out.println("--------------------------------------------------------------"
-            + "----------------------");
-    this.out.println(LINE_BREAKER);
+    this.out.append(LINE_BREAKER + portfolioList.get(portfolioList.size() - 1));
+    this.out.append(LINE_BREAKER + "--------------------------------------------------------------"
+        + "----------------------");
+    this.out.append(LINE_BREAKER);
   }
 
   @Override
   public void alertNoPortfolioMessage() {
-    this.out.println("ALERT: There are no portfolios added yet. Please create portfolios to view."
+    this.out.append(
+        LINE_BREAKER + "ALERT: There are no portfolios added yet. Please create portfolios to view."
             + LINE_BREAKER);
   }
 
   @Override
   public void showAdditionalPortfolioInformation() {
-    this.out.println("* View Portfolio Menu:");
-    this.out.println("1. View composition of a particular portfolio");
-    this.out.println("2. Valuation of Portfolio on a specific date");
-    this.out.println("3. Cost basis of a portfolio till a specific date");
-    this.out.println("4. Purchase a share and add to portfolio");
-    this.out.println("5. Sell a share from portfolio");
-    this.out.println("6. Performance of portfolio over time");
-    this.out.println("Type 'back' to return to Home Menu");
-    this.out.println("Please select an option from 1-x from above: ");
+    this.out.append(LINE_BREAKER + "* View Portfolio Menu:");
+    this.out.append(LINE_BREAKER + "1. View composition of a particular portfolio");
+    this.out.append(LINE_BREAKER + "2. Valuation of Portfolio on a specific date");
+    this.out.append(LINE_BREAKER + "3. Cost basis of a portfolio till a specific date");
+    this.out.append(LINE_BREAKER + "4. Purchase a share and add to portfolio");
+    this.out.append(LINE_BREAKER + "5. Sell a share from portfolio");
+    this.out.append(LINE_BREAKER + "6. Performance of portfolio over time");
+    this.out.append(LINE_BREAKER + "Type 'back' to return to Home Menu");
+    this.out.append(LINE_BREAKER + "Please select an option from 1-x from above: ");
   }
 
   @Override
   public void showValuation(double valuation) {
-    this.out.println("Valuation of Portfolio is:\t$" + valuation + LINE_BREAKER);
+    this.out.append(LINE_BREAKER + "Valuation of Portfolio is:\t$" + valuation + LINE_BREAKER);
   }
 
   @Override
   public void showCostBasis(double valuation) {
-    this.out.println("Cost Basis of Portfolio is:\t$" + valuation + LINE_BREAKER);
+    this.out.append(LINE_BREAKER + "Cost Basis of Portfolio is:\t$" + valuation + LINE_BREAKER);
   }
 
   @Override
   public void selectPortfolio() {
-    this.out.println("Select a 'Portfolio Name' from above list");
+    this.out.append(LINE_BREAKER + "Select a 'Portfolio Name' from above list");
   }
 
   @Override
   public void printInvalidInputMessage() {
-    this.out.println("Invalid Input!");
+    this.out.append(LINE_BREAKER + "Invalid Input!");
   }
 
   @Override
   public void showUploadPortfolioOptions() {
-    this.out.println("Write path to portfolio:");
+    this.out.append(LINE_BREAKER + "Write path to portfolio:");
   }
 
   @Override
   public void askForPortfolioName() {
-    this.out.println("Please write down the unique name of portfolio");
+    this.out.append(LINE_BREAKER + "Please write down the unique name of portfolio");
   }
 
   @Override
   public void notPresentError(String missing) {
-    this.out.println(missing + " is not present, please enter again!");
+    this.out.append(missing + " is not present, please enter again!");
   }
 
   @Override
   public void askForDate() {
-    this.out.println("Please enter date in yyyy-mm-dd format.");
+    this.out.append(LINE_BREAKER + "Please enter date in yyyy-mm-dd format.");
   }
 
   @Override
   public void printCompanyStockUpdated() {
-    this.out.println("This company's number of stocks have been updated!" + LINE_BREAKER);
+    this.out.append(
+        LINE_BREAKER + "This company's number of stocks have been updated!" + LINE_BREAKER);
   }
 
   @Override
   public void developmentInProgress() {
-    this.out.println("This company's data has not been added yet, come back again soon!");
+    this.out.append(
+        LINE_BREAKER + "This company's data has not been added yet, come back again soon!");
   }
 
   @Override
   public void uploadPath() {
-    this.out.println(
-            "How do you want to upload the data? (Write your path seperated by " + FILE_SEPARATOR);
-    this.out.println("1. Absolute Path");
-    this.out.println("2. Relative Path");
-    this.out.println("Type 'back' to return to Home Menu");
+    this.out.append(
+        LINE_BREAKER + "How do you want to upload the data? (Write your path seperated by "
+            + FILE_SEPARATOR);
+    this.out.append(LINE_BREAKER + "1. Absolute Path");
+    this.out.append(LINE_BREAKER + "2. Relative Path");
+    this.out.append(LINE_BREAKER + "Type 'back' to return to Home Menu");
   }
 
   @Override
   public void enterPath() {
-    this.out.println("Enter path:");
+    this.out.append(LINE_BREAKER + "Enter path:");
   }
 
   @Override
   public void printInvalidDateError() {
-    this.out.println("Invalid date format!");
+    this.out.append(LINE_BREAKER + "Invalid date format!");
   }
 
   @Override
   public void printException(String message) {
-    this.out.println(message);
+    this.out.append(message);
   }
 
   @Override
   public void askPortfolioType() {
-    this.out.println("Which type of portfolio do you want to create?");
-    this.out.println("1. Fixed Portfolio");
-    this.out.println("2. Flexible Portfolio");
+    this.out.append(LINE_BREAKER + "Which type of portfolio do you want to create?");
+    this.out.append(LINE_BREAKER + "1. Fixed Portfolio");
+    this.out.append(LINE_BREAKER + "2. Flexible Portfolio");
   }
 
   @Override
   public void alertStockInvalid() {
-    this.out.println("ALERT: Entered stock ticker doesn't exist in this portfolio.");
+    this.out.append(LINE_BREAKER + "ALERT: Entered stock ticker doesn't exist in this portfolio.");
   }
 
   @Override
   public void alertShareNumberInvalid() {
-    this.out.println("ALERT: Entered share number is invalid.");
+    this.out.append(LINE_BREAKER + "ALERT: Entered share number is invalid.");
   }
 
   @Override
   public void showSoldValuation(double valuation) {
-    this.out.println("Earning from selling of Portfolio is:\t$" + valuation);
+    this.out.append(LINE_BREAKER + "Earning from selling of Portfolio is:\t$" + valuation);
   }
 
   @Override
   public void showAmountPaid(double valuation) {
-    this.out.println("Paid amount from buying of Portfolio is:\t$" + valuation);
+    this.out.append(LINE_BREAKER + "Paid amount from buying of Portfolio is:\t$" + valuation);
   }
 
   @Override
   public void printMessage(String message) {
-    this.out.println(message);
+    this.out.append(message);
   }
 
   public void askForEnum(Class<Periodicity> e) {
-    this.out.println("Please type any of the following values to group:");
+    this.out.append(LINE_BREAKER + "Please type any of the following values to group:");
     for (Enum enumValue : EnumSet.allOf(Periodicity.class)) {
-      this.out.println(enumValue.toString());
+      this.out.append(enumValue.toString());
     }
   }
 
@@ -220,24 +225,26 @@ public class ViewImpl implements View {
   public void printStars(LocalDate date, Periodicity periodicity, Double value, int scale) {
     int numStars = (int) Math.ceil(value / Math.pow(10, scale));
     if (periodicity == Periodicity.DAY) {
-      this.out.print(date + ":\t");
+      this.out.append(LINE_BREAKER + date + ":\t");
     } else if (periodicity == Periodicity.MONTH) {
       String day = date.getMonth() + "-" + date.getYear();
-
-      this.out.printf("%-20s :", day);
+      Formatter formatter = new Formatter();
+      formatter.format("%-20s :", day);
+      this.out.append(LINE_BREAKER + formatter);
     } else {
       String day = String.valueOf(date.getYear());
-
-      this.out.printf("%-20s :", day);
+      Formatter formatter = new Formatter();
+      formatter.format("%-20s :", day);
+      this.out.append(LINE_BREAKER + formatter);
     }
     for (int i = 0; i <= numStars; i++) {
-      this.out.print("*");
+      this.out.append("*");
     }
-    this.out.println();
+    this.out.append(LINE_BREAKER);
   }
 
   @Override
   public void alertFixedPortfolio() {
-    this.out.println("This is a fixed portfolio. Cannot sell or add shares to this.");
+    this.out.append(LINE_BREAKER + "This is a fixed portfolio. Cannot sell or add shares to this.");
   }
 }
