23f8ce8714fb0c81d5caf055afdc20c04c8e4dbb
BobErgot <25260745+BobErgot@users.noreply.github.com>
2022-11-29 12:38:40 -0500

    Add upload portfolio GUI

--- src/gui/HomeScreen.java
+++ src/gui/HomeScreen.java
@@ -1,13 +1,38 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Path;
+
 import javax.swing.*;
 
 public class HomeScreen {
-  private JPanel panel1;
-  private JTabbedPane tabbedPane1;
-  private JButton createFixedPortfolioButton;
-  private JButton createFlexiblePortfolioButton;
-  private JTextField textField1;
-  private JTextField textField2;
-  private JButton addShareButton;
+  private JPanel applicationJPanel;
+  private JTabbedPane homeScreenTabbedPane;
+  private JButton createFixedPortfolioJButton;
+  private JButton createFlexiblePortfolioJButton;
+  private JTextField companyTickerJTextField;
+  private JTextField numberSharesJTextField;
+  private JButton addShareJButton;
+  private JButton browseFileJButton;
+  private JLabel notificationJLabel;
+  private JScrollPane viewPortfolioJScrollPane;
+  private JPanel createPortfolioJPanel;
+  private JPanel uploadPortfolioJPanel;
+  private JLabel companyTickerJLabel;
+  private JLabel numberSharesJLabel;
+  private JLabel pathSelectedJLabel;
+
+  public HomeScreen() {
+    browseFileJButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JFileChooser jFileChooser = new JFileChooser();
+        int selectedFile = jFileChooser.showOpenDialog(null);
+        if (selectedFile != JFileChooser.APPROVE_OPTION) return;
+        Path chosenFilePath = jFileChooser.getSelectedFile().toPath();
+      }
+    });
+  }
 }
